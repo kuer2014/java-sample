dac85082758581af4a960f9a28360731c7b5ace8
王帅 <wangshuai@cloud-young.com>
2018-11-16 13:21:59 +0800

    二维码生成工具

--- my-weixin-dev/src/main/java/com/wang/App.java
+++ my-weixin-dev/src/main/java/com/wang/App.java
@@ -3,6 +3,9 @@ package com.wang;
 import com.wang.utils.QRCodeUtil;
 
 import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Dictionary;
 
@@ -14,11 +17,33 @@ import java.util.Dictionary;
  */
 public class App {
     public static void main(String[] args) {
+        createQRCodeFile();
+       // createQRCodeStream();
+    }
 
-     boolean result =QRCodeUtil.createQrCode("https://www.baidu.com/"
-             ,"/Users/wangshuai/Documents/projects/mycode/my-weixin-dev/src/main/resources/qrcode"
-             ,"123"
-     ,".jpg",200,200);
+    private static void createQRCodeFile() {
+        boolean result = QRCodeUtil.createQrCode("https://www.baidu.com/"
+                , "/Users/wangshuai/Documents/projects/mycode/my-weixin-dev/src/main/resources/qrcode"
+                , "test_file2-1"
+                //  , "jpg", 200, 200
+        );
         System.out.println(result);
+    }
+
+    private static void createQRCodeStream() {
+        try {
+            // 第1步、使用File类找到一个文件
+            File f = new File("/Users/wangshuai/Documents/projects/mycode/my-weixin-dev/src/main/resources/qrcode/test1.jpg");    // 声明File对象
+            // 第2步、通过子类实例化父类对象
+            OutputStream out = null;    // 准备好一个输出的对象
+            out = new FileOutputStream(f);    // 通过对象多态性，进行实例化
+            boolean result = QRCodeUtil.createQrCode("https://www.baidu.com/"
+                    , out
+                    //  , "jpg", 200, 200
+            );
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- my-weixin-dev/src/main/java/com/wang/utils/QRCodeUtil.java
+++ my-weixin-dev/src/main/java/com/wang/utils/QRCodeUtil.java
@@ -15,16 +15,19 @@ import java.util.Map;
 
 
 /**
- * Description:
+ * Description:二维码工具类
  * All Rights Reserved.
  *
  * @version 1.0  2018/10/20 16:15  by  王帅（wangshuai@cloud-young.com）创建
  */
 public class QRCodeUtil {
-//    public static OutputStream buildCode(String url) {
-//        ByteArrayOutputStream out = QRCode.from(url).to(ImageType.PNG).stream();
-//        return out;
-//    }
+
+    private static final int BLACK = 0xFF000000;
+    private static final int WHITE = 0xFFFFFFFF;
+    private static final String CHARACTER_SET = "UTF-8";
+    private static final String DEFAULT_FORMAT = "jpg";
+    private static final int DEFAULT_WIDTH = 400;
+    private static final int DEFAULT_HEIGHT = 400;
 
     /**
      * @param url      网址
@@ -35,27 +38,28 @@ public class QRCodeUtil {
      * @version 1.0  2018/10/20 17:03   by  王帅（wangshuai@cloud-young.com）创建
      */
     public static boolean createQrCode(String url, String filePath, String fileName) {
-        return createQrCode(url, filePath, fileName, "jpg", 400, 400);
+        return createQrCode(url, filePath, fileName, DEFAULT_FORMAT, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
+
     /**
      * @param url      网址
      * @param filePath 二维码文件保存路径
      * @param fileName 二维码文件名称
-     *  @param extension 二维码文件格式；jpg/png/bmp 等。
-     * @param width 二维码文件宽度
-     * @param hight 二维码文件高度
+     * @param format   二维码文件格式；jpg/png/bmp 等。
+     * @param width    二维码文件宽度
+     * @param height   二维码文件高度
      * @return 成功返回true
      * @Descpription:生成指定网址的二维码文件，并保存在指定路径下
      * @version 1.0  2018/10/20 17:03   by  王帅（wangshuai@cloud-young.com）创建
      */
-    public static boolean createQrCode(String url, String filePath, String fileName, String extension, int width, int hight) {
+    public static boolean createQrCode(String url, String filePath, String fileName, String format, int width, int height) {
         try {
             Map<EncodeHintType, String> hints = new HashMap<EncodeHintType, String>();
-            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-            BitMatrix bitMatrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, hight, hints);
-            File file = new File(filePath, fileName+"."+extension);
+            hints.put(EncodeHintType.CHARACTER_SET, CHARACTER_SET);
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height, hints);
+            File file = new File(filePath, fileName + "." + format);
             if (file.exists() || ((file.getParentFile().exists() || file.getParentFile().mkdirs()) && file.createNewFile())) {
-                writeToFile(bitMatrix, extension, file);
+                writeToFile(bitMatrix, format, file);
                 //System.out.println("成功生成二维码：" + file);
                 return true;
             }
@@ -75,11 +79,35 @@ public class QRCodeUtil {
     }
 
     /**
-     * @param
+     * @param stream 存储二维码文件的流
+     * @return
+     * @Descpription:生成的二维码写入一个给定的流中
+     * @version 1.0  2018/10/22 18:27   by  王帅（wangshuai@cloud-young.com）创建
+     */
+    public static boolean createQrCode(String url, OutputStream stream) {
+        return createQrCode(url, stream, DEFAULT_FORMAT, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    /**
+     * @param stream 存储二维码文件的流
      * @return
      * @Descpription:生成的二维码写入一个给定的流中
      * @version 1.0  2018/10/20 17:03   by  王帅（wangshuai@cloud-young.com）创建
      */
+    public static boolean createQrCode(String url, OutputStream stream, String format, int width, int height) {
+        try {
+            Map<EncodeHintType, String> hints = new HashMap<EncodeHintType, String>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height, hints);
+            writeToStream(bitMatrix, format, stream);
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     static void writeToStream(BitMatrix matrix, String format, OutputStream stream) throws IOException {
         BufferedImage image = toBufferedImage(matrix);
         if (!ImageIO.write(image, format, stream)) {
@@ -87,19 +115,35 @@ public class QRCodeUtil {
         }
     }
 
-    private static final int BLACK = 0xFF000000;
-    private static final int WHITE = 0xFFFFFFFF;
-
     private static BufferedImage toBufferedImage(BitMatrix matrix) {
         int width = matrix.getWidth();
         int height = matrix.getHeight();
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        // BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = loadImage();
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 image.setRGB(x, y, matrix.get(x, y) ? BLACK : WHITE);
             }
         }
         return image;
+    }
+
+    //11.1测试
+    private static BufferedImage loadImage() {
+        //Path file = new Path(); BASE_IMAGE_PATH.resolve(fileName);
+//        Path fpath2 = Paths.get(fileName);
+//        if (!Files.exists(file)) {
+//            // try starting with 'core' since the test base is often given as the project root
+//            file = Paths.get("core/").resolve(BASE_IMAGE_PATH).resolve(fileName);
+//        }
+        try {
+            File file = new File("/Users/wangshuai/Documents/projects/mycode/my-weixin-dev/src/main/resources/qrcode/test_file2.jpg");
+            return ImageIO.read(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
 }
