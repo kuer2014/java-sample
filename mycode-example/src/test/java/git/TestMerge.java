3e5abd5e419e815a876a93c251a0a8b63e2aeb15
王帅 <wangshuai@cloud-young.com>
2018-11-30 14:11:15 +0800

    master提交1

---
+++ mycode-example/src/test/java/git/TestMerge.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * Description:
+ * All Rights Reserved.
+ *
+ * @version 1.0  2018/11/30 14:07  by  王帅（wangshuai@cloud-young.com）创建
+ */
+public class TestMerge {
+    String branchMaster="1";
+    String branchDev="1";
+}
