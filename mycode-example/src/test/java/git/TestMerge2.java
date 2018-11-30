76e1ad3525d58dea019f8161d8d23b33b5f6aa6f
王帅 <wangshuai@cloud-young.com>
2018-11-30 15:55:12 +0800

    dev2提交

--- mycode-example/src/test/java/git/TestMerge2.java
+++ mycode-example/src/test/java/git/TestMerge2.java
@@ -7,9 +7,6 @@ package git;
  * @version 1.0  2018/11/30 14:15  by  王帅（wangshuai@cloud-young.com）创建
  */
 public class TestMerge2 {
-    String branchMaster="2";
-    String branchDev="2";
-    String branchDev3="3";
-    String branchDev2="dev2";
+String aaa="1";
 
 }
