09df4028075836f49d1335f5be1d6ee863088076
王帅 <wangshuai@cloud-young.com>
2019-01-27 16:01:09 +0800

    servlet 示例

---
+++ my-web/src/main/java/MyServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Description:
+ * All Rights Reserved.
+ *
+ * @version 1.0  2018/8/14 12:01  by  王帅（wangshuai@cloud-young.com）创建
+ */
+public class MyServlet implements Servlet {
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+}
