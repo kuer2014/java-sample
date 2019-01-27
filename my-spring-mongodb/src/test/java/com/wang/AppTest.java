d51bad93ce12fa6c406ca0f2edf7485be0d7232c
王帅 <wangshuai@cloud-young.com>
2019-01-27 10:21:43 +0800

    mongodb 示例补充

--- my-spring-mongodb/src/test/java/com/wang/AppTest.java
+++ my-spring-mongodb/src/test/java/com/wang/AppTest.java
@@ -1,5 +1,6 @@
 package com.wang;
 
+import com.wang.model.ClueDistributionVo;
 import com.wang.model.TestM;
 import com.wang.model.UserBehaviorDataVo;
 import org.junit.Test;
@@ -13,10 +14,14 @@ import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.aggregation.ConditionalOperators;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+
+import java.util.Date;
+import java.util.List;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
@@ -42,10 +47,16 @@ public class AppTest {
     public void Test2(){
         System.out.println(testM.getName());
     }
+    /**
+     * @Descpription: 查询列表
+     * @version 1.0  2019-01-25 18:55   by  王帅（wangshuai@cloud-young.com）创建
+     * @param
+     * @return
+     */
     @Test
     public void test1(){
         Aggregation aggregation = newAggregation(
-                match(Criteria.where("source").is(1)),
+                match(Criteria.where("source").is(1)),  //查询条件
                 project("createTimeHour", "createTimeDay", "clueInfo", "platformType")
                         .and("carInfo.serialName").as("serialName")
                         .and("dealerInfo.organizationType").as("organizationType")
@@ -81,5 +92,48 @@ public class AppTest {
         AggregationResults<UserBehaviorDataVo> result = mongoTemplate.aggregate(aggregation, collectionName, UserBehaviorDataVo.class);
       //  afterHandler(result.getMappedResults());
         System.out.println( result.getMappedResults().get(0));
+    }
+/**
+ * @Descpription:查询2
+ * @version 1.0  2019-01-25 18:56   by  王帅（wangshuai@cloud-young.com）创建
+ * @param
+ * @return
+ */
+    @Test
+    public void test2(){
+        Aggregation aggregation = newAggregation(
+                match(getCriteria(new Date(),new Date())),
+                match(Criteria.where("operationType").is(400)),
+                project( "clueInfo"),
+                group().addToSet("clueInfo.mobile").as("clueMobileSet"),
+                project().andExclude("_id").and("clueMobileSet").size().as("clueCount")
+        );
+        AggregationResults<ClueDistributionVo> aggregateResult = mongoTemplate.aggregate(aggregation, collectionName, ClueDistributionVo.class);
+        List<ClueDistributionVo> result = aggregateResult.getMappedResults();
+        System.out.println((long)result.get(0).getClueCount());
+    }
+    /**
+     * 获取过滤条件
+     * @return
+     */
+    private Criteria getCriteria(Date startDate,Date endDate) {
+        Criteria criteria = Criteria.where("source").is(1);
+            criteria.and("dealerInfo.dealerId").is(10000);
+            criteria.and("createTimeDay").gte(startDate.getTime()).lte(endDate.getTime());
+        return criteria;
+    }
+    /**
+     * @Descpription:聚合查询
+     * @version 1.0  2019-01-25 19:07   by  王帅（wangshuai@cloud-young.com）创建
+     * @param
+     * @return
+     */
+    @Test
+    public void test3(){
+        Query query = new Query();
+        query.addCriteria(getCriteria(new Date(),new Date()));
+        query.addCriteria(Criteria.where("operationType").in(100));
+        long count= mongoTemplate.count(query, collectionName);
+        System.out.println(count);
     }
 }
