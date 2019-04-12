60356e6915c56140ce00027e8ffc2233d9a2d761
王帅 <wangshuai@cloud-young.com>
2019-04-12 16:02:21 +0800

    redis示例完善

--- my-redis/src/main/java/com/wang/JedisApp.java
+++ my-redis/src/main/java/com/wang/JedisApp.java
@@ -1,6 +1,11 @@
 package com.wang;
 
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Description:
@@ -10,11 +15,29 @@ import redis.clients.jedis.Jedis;
  */
 public class JedisApp {
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("localhost",6379);
+        Jedis jedis = new Jedis("localhost", 6379);
         jedis.auth("111");
         jedis.set("foo", "bar");
-        jedis.lpush("jedis","j","e","d");
+        jedis.lpush("jedis", "j", "e", "d");
         String value = jedis.get("foo");
         System.out.println(value);
+        //集群
+        Set<HostAndPort> nodes = new HashSet<>();
+        nodes.add(new HostAndPort("localhost", 7001));
+        nodes.add(new HostAndPort("localhost", 7002));
+        nodes.add(new HostAndPort("localhost", 7003));
+        nodes.add(new HostAndPort("localhost", 7004));
+        nodes.add(new HostAndPort("localhost", 7005));
+        nodes.add(new HostAndPort("localhost", 7006));
+        JedisCluster jedisCluster = new JedisCluster(nodes);
+        // 直接使用JedisCluster对象操作redis。
+        jedisCluster.set("test", "123");
+        String string = jedisCluster.get("test");
+        System.out.println(string);
+        try {
+            jedisCluster.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
